7604daa3872d8c88a5d10498a9aa6ba031b0fb30
toxeeec <bartosz.kapciak@gmail.com>
2023-11-18 00:58:36 +0100

    feat: add tailwind

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 export default {
-	plugins: ["prettier-plugin-astro"],
+	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
 	printWidth: 120,
 	tabWidth: 4,
 	useTabs: true,
